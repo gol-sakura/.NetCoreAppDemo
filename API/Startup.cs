d60cd36326f25f5e72d15d3c20f36bead6f22f3e
Sakura <golsa_n@outlook.com>
2020-12-13 19:34:15 +0100

    EndOfSection3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,12 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
             
                 
@@ -53,10 +59,11 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
-
+            
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
